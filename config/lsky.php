109306cdfb6c4160da1259e73387bdf974f7b959
gaopf <1241961245@qq.com>
2025-09-30 18:20:46 +0800

    feat: upt

--- config/lsky.php
+++ config/lsky.php
@@ -11,7 +11,7 @@ return [
     */
 
     // 多文件上传配置
-    'max_upload_files' => env('LSKY_MAX_UPLOAD_FILES', 10),
+    'max_upload_files' => env('LSKY_MAX_UPLOAD_FILES', 100),
     'max_file_size' => env('LSKY_MAX_FILE_SIZE', 10240), // KB
     'allowed_extensions' => env('LSKY_ALLOWED_EXTENSIONS', 'jpg,jpeg,png,gif,webp,bmp,ico,svg'),
 ];
